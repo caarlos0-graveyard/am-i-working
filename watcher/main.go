fb402fb50d70a9cc049df55443be572ab16939a5
Carlos Alexandro Becker <caarlos0@gmail.com>
2016-06-08 17:32:04 -0300

    refactory

--- watcher/main.go
+++ watcher/main.go
@@ -1,8 +1,11 @@
 package watcher
 
 import (
+	"bufio"
 	"errors"
 	"log"
+	"os"
+	"regexp"
 
 	"gopkg.in/fsnotify.v1"
 )
@@ -12,6 +15,10 @@ func Watch(domain string) error {
 		return errors.New("Invalid domain")
 	}
 	log.Println("Watching for domain", domain)
+	return watch(domain)
+}
+
+func watch(domain string) error {
 	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
 		return err
@@ -22,16 +29,40 @@ func Watch(domain string) error {
 		return err
 	}
 
+	regex := regexp.MustCompile("domain .*(" + domain + ").*")
+	return loop(domain, regex, watcher)
+}
+
+func loop(
+	domain string,
+	regex *regexp.Regexp,
+	watcher *fsnotify.Watcher,
+) error {
 	for {
 		select {
 		case event := <-watcher.Events:
 			if event.Op&fsnotify.Write == fsnotify.Write {
-				log.Println("File Modified:", event.Name)
-				//regex
-				//log se bate na regex
+				if err := logDomain(event, regex); err != nil {
+					return err
+				}
 			}
 		case err := <-watcher.Errors:
 			log.Println(err)
 		}
 	}
+}
+
+func logDomain(event fsnotify.Event, regex *regexp.Regexp) error {
+	file, err := os.Open(event.Name)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		if matches := regex.FindStringSubmatch(scanner.Text()); len(matches) > 0 {
+			log.Println("Into domain", matches[1])
+		}
+	}
+	return nil
 }
