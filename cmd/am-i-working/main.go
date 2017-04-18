0569e75940d7ab3575f1710a5cd6afac208e8266
Carlos Alexandro Becker <caarlos0@gmail.com>
2017-04-18 09:35:20 -0300

    moved

---
+++ cmd/am-i-working/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/caarlos0/am-i-working/cmd/am-i-working/commands"
+	"github.com/urfave/cli"
+)
+
+var version = "master"
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "am-i-working"
+	app.Usage = "Logs your working activity based on /etc/resolv.conf domain"
+	app.Version = version
+	app.Author = "Carlos Alexandro Becker <@caarlos0>"
+	app.Copyright = "MIT"
+	app.Commands = []cli.Command{
+		commands.Watch,
+		commands.CreateService,
+		commands.RestartService,
+		commands.StartService,
+		commands.StopService,
+		commands.DeleteService,
+	}
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
